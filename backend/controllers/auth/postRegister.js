93629f13dae90f76125c6496f8b0c4073cea3cef
Yurii Panasuik <thoryur@gmail.com>
2025-03-02 18:39:57 +0200

    postRegister

--- backend/controllers/auth/postRegister.js
+++ backend/controllers/auth/postRegister.js
@@ -1,5 +1,41 @@
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
 const postRegister = async (req, res) => {
-    res.send('register route');
+    //res.send('register route');
+    try {
+        const { username, mail, password } = req.body;
+
+        // check - user exist
+        const userExist = await User.exists({ mail });
+
+        if (userExist) {
+            return res.status(409).send('e-mail already in use');
+        }
+
+        // encrypt the password
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        // create and save user document
+        const user = await User.create({
+            username,
+            mail: mail.toLowerCase(),
+            password: encryptedPassword
+        })
+
+        // create JWT token
+        const token = 'JWT TOKEN';
+
+        res.status(201).json({
+            userDetails: {
+                username: user.username,
+                mail: user.mail,
+                token: token,
+            }
+        });
+
+    } catch (err) {
+        return res.status(500).send("Try again later");
+    }
 };
 
 module.exports = postRegister;
