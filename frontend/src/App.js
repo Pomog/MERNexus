94e7630799d5e4b34fd7d9d52d95059e2a28ab0e
Yurii Panasuik <thoryur@gmail.com>
2025-03-20 20:55:18 +0200

    legacy_createStore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate
+} from 'react-router-dom';
+import LoginPage from "./authPages/loginPage/LoginPage";
+import RegisterPage from "./authPages/registerPage/RegisterPage";
+import DashBoard from "./dashBoard/DashBoard";
 import './App.css';
 
 function App() {
-  return <div className="App">
-      Hello React
-    </div>;
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/dashboard" element={<DashBoard />} />
+                    <Route path="*" element={<Navigate replace to="/dashboard" />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,14 +1,17 @@
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { combineReducers, createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { combineReducers, legacy_createStore as createStore, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
-
 import authReducer from "./reducers/authReducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
-
 });
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+);
 
 export default store;
