f28d0e5bc888839d61ccab2bf3ded9e61fbcfa98
Yurii Panasuik <thoryur@gmail.com>
2025-03-22 23:54:12 +0200

    LoginPage

---
+++ frontend/src/authPages/loginPage/LoginPageHeader.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Typography } from '@mui/material';
+
+const LoginPageHeader = () => {
+    return (
+        <>
+            <Typography variant='h5' sx={{
+                color: "white"
+            }}>
+                Welcome Back!
+            </Typography>
+            <Typography sx={{
+                color: "#b9bbbe"
+            }}>
+                We are happy, you with us!
+            </Typography>
+        </>
+    )
+}
+
+export default LoginPageHeader;
