41d4c9a44efa6744e6c424f3483ebe9d4390ba80
Yurii Panasuik <thoryur@gmail.com>
2025-03-26 18:43:43 +0200

    Tooltip

--- frontend/src/authPages/loginPage/LoginPageFooter.js
+++ frontend/src/authPages/loginPage/LoginPageFooter.js
@@ -2,6 +2,16 @@ import React from 'react';
 import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
 import RedirectInfo from "../../shared/components/RedirectInfo";
 import { useNavigate } from 'react-router-dom';
+import { Tooltip } from "@mui/material";
+
+const getFormNotValidMessage = () => {
+    return 'Enter correct e-mail and password (6-12 char)';
+}
+
+const getFormValidMessage = () => {
+    return 'Press to log in!';
+}
+
 
 const LoginPageFooter = ({ handleLogin, isFormValid }) => {
 
@@ -12,6 +22,11 @@ const LoginPageFooter = ({ handleLogin, isFormValid }) => {
 
     return (
         <>
+        <Tooltip
+            title={!isFormValid ?
+                getFormNotValidMessage()
+                :
+                getFormValidMessage()}>
         <div>
             <CustomPrimaryButton
                 label="Log in"
@@ -20,6 +35,7 @@ const LoginPageFooter = ({ handleLogin, isFormValid }) => {
                 onClick={handleLogin}
             />
         </div>
+        </Tooltip>
         <RedirectInfo
             text='Need an account? '
             redirectText='Create an account'
