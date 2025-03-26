8ca5f6d9603ab256e43a10be731ee3c62d85106d
Yurii Panasuik <thoryur@gmail.com>
2025-03-26 20:42:06 +0200

    validateLoginForm

--- frontend/src/authPages/loginPage/LoginPage.js
+++ frontend/src/authPages/loginPage/LoginPage.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthBox from '../../shared/components/AuthBox';
 import LoginPageHeader from './LoginPageHeader';
 import LoginPageInputs from "./LoginPageInputs";
 import LoginPageFooter from "./LoginPageFooter";
+import {validateLoginForm} from "../../shared/utils/validators";
 
 const LoginPage = () => {
     const [mail, setMail] = useState('');
@@ -11,6 +12,11 @@ const LoginPage = () => {
     const handleLogin = () => {
         console.log("log IN");
     };
+
+    useEffect(() => {
+        setIsFormValid(validateLoginForm({ mail, password}));
+        console.log(validateLoginForm({ mail, password}));
+    }, [mail, password, setIsFormValid]);
 
     return (
         <AuthBox>
