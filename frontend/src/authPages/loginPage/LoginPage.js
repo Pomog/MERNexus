37c6b0d2adc2fe1031f3192e38b86a85198db990
Yurii Panasuik <thoryur@gmail.com>
2025-03-26 18:36:48 +0200

    login form

--- frontend/src/authPages/loginPage/LoginPage.js
+++ frontend/src/authPages/loginPage/LoginPage.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import AuthBox from '../../shared/components/AuthBox';
 import LoginPageHeader from './LoginPageHeader';
 import LoginPageInputs from "./LoginPageInputs";
+import LoginPageFooter from "./LoginPageFooter";
 
 const LoginPage = () => {
     const [mail, setMail] = useState('');
     const [password, setPassword] = useState('');
+    const [isFormValid, setIsFormValid] = useState(false);
+    const handleLogin = () => {
+        console.log("log IN");
+    };
 
     return (
         <AuthBox>
@@ -15,6 +20,10 @@ const LoginPage = () => {
                 setMail={setMail}
                 password={password}
                 setPassword={setPassword}
+            />
+            <LoginPageFooter
+                isFormValid={isFormValid}
+                handleLogin={handleLogin}
             />
         </AuthBox>
     );
---
+++ frontend/src/authPages/loginPage/LoginPageFooter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
+import RedirectInfo from "../../shared/components/RedirectInfo";
+import { useNavigate } from 'react-router-dom';
+
+const LoginPageFooter = ({ handleLogin, isFormValid }) => {
+
+    const history = useNavigate ();
+    const handlePushToRegisterPage = () => {
+        history("/register");
+    };
+
+    return (
+        <>
+        <div>
+            <CustomPrimaryButton
+                label="Log in"
+                additionalStyles={{marginTop: "30px"}}
+                disabled={!isFormValid}
+                onClick={handleLogin}
+            />
+        </div>
+        <RedirectInfo
+            text='Need an account? '
+            redirectText='Create an account'
+            additionalStyles={{ marginTop: '5px'}}
+            redirectHandler={handlePushToRegisterPage}
+        />
+        </>
+    );
+};
+
+export default LoginPageFooter;
