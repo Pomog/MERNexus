84ff4f029d6da975dece47f176bb5a28b6b92965
Yurii Panasuik <thoryur@gmail.com>
2025-04-20 00:19:59 +0300

    FriendsList

--- frontend/src/dashBoard/friendsSidebar/friendsList/FriendsList.js
+++ frontend/src/dashBoard/friendsSidebar/friendsList/FriendsList.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
+import FriendsListItem from "./FriendsListItem";
+
+/**
+ * @typedef {{ id: number, username: string, isOnline: boolean }} Friend
+ */
+
+/** @type {Friend[]} */
+const DUMMY_FRIENDS = [
+    {
+        id: 1,
+        username: 'Yurii',
+        isOnline: true,
+    },
+    {
+        id: 2,
+        username: 'Andrey',
+        isOnline: true,
+    },
+    {
+        id: 3,
+        username: 'Nelya',
+        isOnline: false,
+    },
+];
 
 const MainContainer =
     styled('div')(({ theme }) => ({
@@ -10,7 +34,14 @@ const MainContainer =
 const FriendsList = () => {
     return (
         <MainContainer>
-            
+            {DUMMY_FRIENDS.map( (f) => (
+                <FriendsListItem
+                    username={f.username}
+                    id={f.id}
+                    key={f.id}
+                    isOnline={f.isOnline}
+                />
+            ))}
         </MainContainer>
     );
 };
