a23fc7ec49f0bcf0e63e4dbe3bfe8984b04705f6
Yurii Panasuik <thoryur@gmail.com>
2025-04-22 15:32:21 +0300

    PendingInvitationList

--- frontend/src/dashBoard/friendsSidebar/pendingInvitationsList/PendingInvitationList.js
+++ frontend/src/dashBoard/friendsSidebar/pendingInvitationsList/PendingInvitationList.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
+import PendingInvitationListItem from "./PendingInvitationListItem";
+
+const DUMMY_INVITATION = [
+    {
+        _id: '1',
+        senderId: {
+            username: 'Anna',
+            mail: 'test1@mail.com',
+        },
+    },
+    {
+        _id: '2',
+        senderId: {
+            username: 'Bob',
+            mail: 'test2@mail.com',
+        },
+    },
+]
 
 const MainContainer =
     styled('div')(({ theme }) => ({
@@ -12,9 +30,20 @@ const MainContainer =
     }));
 
 const PendingInvitationList = () => {
+    /**
+     * @type {import('react').ReactElement[]}
+     */
+    const invitationElements = DUMMY_INVITATION.map(inv => (
+        <PendingInvitationListItem
+            key={inv._id}
+            id={inv._id}
+            username={inv.senderId.username}
+            mail={inv.senderId.mail}
+        />
+    ));
     return (
         <MainContainer>
-            
+            {invitationElements}
         </MainContainer>
     );
 };
