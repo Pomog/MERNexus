b6bec82381133bef2dedbb3184ec8981c8b0e50e
Yurii Panasuik <thoryur@gmail.com>
2025-05-09 22:08:25 +0300

    Initial Update Of Pending Invitations

--- backend/socketHandlers/newConnectionHandler.js
+++ backend/socketHandlers/newConnectionHandler.js
@@ -1,4 +1,5 @@
 const {addNewConnectedUser} = require("../serverStore");
+const {updateFriendPendingInvitation} = require("./updates/friends");
 const newConnectionHandler = async (socket, io) => {
     const userDetails = socket.user;
 
@@ -6,6 +7,9 @@ const newConnectionHandler = async (socket, io) => {
         socketId: socket.id,
         userId: userDetails.userId,
     });
+
+    // update pending friends invitations list
+    await updateFriendPendingInvitation(userDetails.userId);
 };
 
 module.exports = newConnectionHandler;
