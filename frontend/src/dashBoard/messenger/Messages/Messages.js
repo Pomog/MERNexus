443691ebd51c32f609e0e73fdaf432e33288f2c1
Yurii Panasuik <thoryur@gmail.com>
2025-05-25 21:15:29 +0300

    Message Header

--- frontend/src/dashBoard/messenger/Messages/Messages.js
+++ frontend/src/dashBoard/messenger/Messages/Messages.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import MessagesHeader from './MessagesHeaader';
+import {connect} from "react-redux";
+import {styled} from "@mui/system";
 
-const Messages = () => {
+const MainContainer = styled('div')({
+    height: 'calc(100% - 60px)',
+    overflow: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+});
+
+const Messages = ({ chosenChatDetails, messages }) => {
     return (
-        <div>
-            
-        </div>
+        <MainContainer>
+            <MessagesHeader
+                name={chosenChatDetails?.name}
+            />
+        </MainContainer>
     );
 };
 
-export default Messages;
+const mapStoreStateToProps = ({ chat }) => {
+    return {
+        ...chat,
+    };
+};
+
+export default connect(mapStoreStateToProps)(Messages);
