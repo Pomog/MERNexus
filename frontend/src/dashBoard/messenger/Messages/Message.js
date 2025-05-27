524d57d8051c90999b77392a684fc2e825af590f
Yurii Panasuik <thoryur@gmail.com>
2025-05-27 22:24:00 +0300

    Rendering Single Messages

---
+++ frontend/src/dashBoard/messenger/Messages/Message.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {styled} from "@mui/system";
+import Avatar from "../../../shared/components/Avatar";
+import {Typography} from "@mui/material";
+
+const MainContainer = styled('div')({
+    widths: '97%',
+    display: 'flex',
+    marginTop: '10px',
+});
+
+const AvatarContainer = styled('div')({
+    width: '70px',
+});
+
+const MessageContainer = styled('div')({
+    display: 'flex',
+    flexDirection: 'column'
+});
+
+const MessageContent = styled('div')({
+    color: '#DCDDDE',
+});
+
+const SameAuthorMessageContent = styled('div')({
+    color: '#DCDDDE',
+    width: '97%',
+})
+
+const SameAuthorMessageText = styled('span')({
+    marginLeft: '70px',
+});
+
+const Message = ({content, sameAuthor, userName, date, sameDay}) => {
+    if (sameAuthor && sameDay) {
+        return (
+            <SameAuthorMessageContent>
+                <SameAuthorMessageText>{content}</SameAuthorMessageText>
+            </SameAuthorMessageContent>
+        );
+    }
+    return (
+        <MessageContainer>
+            <AvatarContainer>
+                <Avatar userName={userName}/>
+            </AvatarContainer>
+            <MessageContainer>
+                <Typography style={{
+                    fontSize: '16px', color: 'white'
+                }}>
+                    {userName}{' '}
+                    <span style={{ fontSize: '12px', color: '#72767D'}}>
+                        {date}
+                    </span>
+                </Typography>
+                <MessageContent>
+                    {content}
+                </MessageContent>
+            </MessageContainer>
+
+        </MessageContainer>
+    );
+};
+
+export default Message;
--- frontend/src/dashBoard/messenger/Messages/Messages.js
+++ frontend/src/dashBoard/messenger/Messages/Messages.js
@@ -2,6 +2,8 @@ import React from 'react';
 import MessagesHeader from './MessagesHeaader';
 import {connect} from "react-redux";
 import {styled} from "@mui/system";
+import DUMMY_MESSAGES from "./DummyMewssage";
+import Message from './Message';
 
 const MainContainer = styled('div')({
     height: 'calc(100% - 60px)',
@@ -17,6 +19,17 @@ const Messages = ({ chosenChatDetails, messages }) => {
             <MessagesHeader
                 name={chosenChatDetails?.name}
             />
+            {DUMMY_MESSAGES.map((message,index) => {
+                return <Message
+                    key={message._id}
+                    content={message.content}
+                    userName={message.author.userName}
+                    date={message.date}
+                    sameDay={message.sameDay}
+
+                />
+
+            })}
         </MainContainer>
     );
 };
