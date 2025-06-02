1e2126357c0159e267cd14fcf4d6041c30202b48
Yurii Panasuik <thoryur@gmail.com>
2025-06-02 20:36:46 +0300

    Realtime Chat Updates

--- backend/socketHandlers/directMessageHandler.js
+++ backend/socketHandlers/directMessageHandler.js
@@ -4,14 +4,11 @@ const directMessageHandler = async (soket, data) => {
     try {
         console.log("directMessageHandler");
 
-        const { userId } = soket.user;
-        const { receiverUserId, content } = data;
+        const {userId} = soket.user;
+        const {receiverUserId, content} = data;
 
         const message = await Message.create({
-            content: content,
-            authorId: userId,
-            date: new Date(),
-            type: 'DIRECT',
+            content: content, authorId: userId, date: new Date(), type: 'DIRECT',
         });
 
         const conversation = await Conversation.findOne({
@@ -23,15 +20,16 @@ const directMessageHandler = async (soket, data) => {
         if (conversation) {
             conversation.messages.push(message._id);
             await conversation.save();
+
+            chatUpdates.updateChatHistory(conversation._id.toString());
         } else {
             const newConversation = await Conversation.create({
-                messages: [message._id],
-                participants: [userId, receiverUserId],
-            })
+                messages: [message._id], participants: [userId, receiverUserId],
+            });
 
+            chatUpdates.updateChatHistory(conversation._id.toString());
 
         }
-
     } catch (err) {
         console.log(err);
     }
---
+++ backend/socketHandlers/updates/chat.js
@@ -0,0 +1,41 @@
+const Conversation = require('../../models/conversation');
+const serverStore = require('../../serverStore');
+
+const updateChatHistory = async (
+    conversationId,
+    toSpecifiedSocketId = null
+) => {
+    const conversation = await Conversation.findById(conversationId).populate({
+        path: 'messages',
+        model: 'Model',
+        populate: {
+            path: 'author',
+            model: 'User',
+            select: 'userName _id'
+        },
+    });
+    if (conversation) {
+        const io = serverStore.getSocketServerInstance();
+
+        if (toSpecifiedSocketId) {
+            // initial update
+            return io.to(toSpecifiedSocketId).emit('direct-chat-history', {
+                messages: conversation.messages,
+                participants: conversation.participants,
+            });
+        }
+
+        conversation.participants.forEach(userId => {
+            const activeConnections = serverStore.getActiveConnections(userId.toString());
+
+            activeConnections.forEach(socketId => {
+                io.to(socketId).emit('direct-chat-history', {
+                    messages: conversation.messages,
+                    participants: conversation.participants,
+                });
+            });
+        })
+    }
+};
+
+module.exports = { updateChatHistory };
