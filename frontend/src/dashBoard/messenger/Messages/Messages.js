559cf0f2b7447de440ace9859fb383325a21926d
Yurii Panasuik <thoryur@gmail.com>
2025-06-07 21:15:12 +0300

    Rendering Real Messages

--- frontend/src/dashBoard/messenger/Messages/Messages.js
+++ frontend/src/dashBoard/messenger/Messages/Messages.js
@@ -14,19 +14,39 @@ const MainContainer = styled('div')({
 });
 
 const Messages = ({ chosenChatDetails, messages }) => {
+    const convertDateToHumanReadable = (date, format) => {
+        const map = {
+            mm: date.getMonth() + 1,
+            dd: date.getDate(),
+            yy: date.getFullYear().toString().slice(-2),
+            yyyy: date.getFullYear(),
+        };
+
+        return format.replace(/mm|dd|yy|yyy/gi, (matched) => map[matched]);
+    };
+
     return (
         <MainContainer>
             <MessagesHeader
                 name={chosenChatDetails?.name}
             />
-            {DUMMY_MESSAGES.map((message) => {
+            {messages.map((message, index) => {
+                const sameAuthor = index > 0 &&
+                    messages[index].author._id === messages[index-1].author._id;
+                const sameDay = index > 0 &&
+                    convertDateToHumanReadable(new Date(message.date), "dd/mm/yy") ===
+                    convertDateToHumanReadable(new Date(messages[index-1].date), "dd/mm/yy");
+
+                console.log(messages);
+
                 return <Message
                     messageId={message._id}
                     content={message.content}
                     userName={message.author.userName}
-                    date={message.date}
-                    sameDay={message.sameDay}
-                    sameAuthor={message.sameAuthor}
+                    date={convertDateToHumanReadable(
+                        new Date(message.date), "dd/mm/yy")}
+                    sameDay={sameDay}
+                    sameAuthor={sameAuthor}
                 />
 
             })}
