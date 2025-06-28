c9fe1ef54de1e5cfcb84de4ecae9afe78353eb49
Yurii Panasuik <thoryur@gmail.com>
2025-06-28 22:16:36 +0300

    Leaving Room at Disconnect Event

--- backend/socketHandlers/disconnectHandler.js
+++ backend/socketHandlers/disconnectHandler.js
@@ -1,6 +1,19 @@
 const serverStore = require('../serverStore');
+const roomLeaveHandler = require("./roomLeaveHandler");
 
-const disconnectHandler = (socket) => {
+const disconnectHandler = (socket, data) => {
+    const activeRooms = serverStore.getActiveRooms();
+
+    activeRooms.forEach(activeRoom => {
+        const userInRoom = activeRoom.participants.some(
+            (participant) => participant.socketId === socket.id
+        );
+
+        if (userInRoom) {
+            roomLeaveHandler(socket, { roomToLeave: activeRoom.roomId})
+        }
+    });
+
     serverStore.removeConnectedUser(socket.id);
 }
 
--- backend/socketHandlers/roomLeaveHandler.js
+++ backend/socketHandlers/roomLeaveHandler.js
@@ -2,19 +2,9 @@ const serverStore = require( "../serverStore");
 const roomUpdates = require("./updates/rooms");
 
 const roomLeaveHandler = (socket, data) => {
-
-    console.log("data");
-    console.log(data);
-
     const { roomToLeave } = data;
 
-    console.log("roomId");
-    console.log(roomToLeave);
-
     const activeRoom = serverStore.getActiveRoom(roomToLeave);
-
-    console.log("activeRoom");
-    console.log(activeRoom);
 
     if(activeRoom) {
         serverStore.leaveActiveRoom(roomToLeave, socket.id);
