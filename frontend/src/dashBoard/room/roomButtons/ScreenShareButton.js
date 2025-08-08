32785f9c5a29ee1c5a676c8e46199688c695aa4e
Yurii Panasuik <thoryur@gmail.com>
2025-08-08 23:27:58 +0300

    Screen Sharing

--- frontend/src/dashBoard/room/roomButtons/ScreenShareButton.js
+++ frontend/src/dashBoard/room/roomButtons/ScreenShareButton.js
@@ -2,17 +2,42 @@ import React, {useState} from 'react';
 import {IconButton} from "@mui/material";
 import ScreenShareIcon from "@mui/icons-material/ScreenShare";
 import StopScreenShareIcon from "@mui/icons-material/StopScreenShare";
+import {setScreenSharingStream} from "../../../store/actions/roomActions";
 
-const ScreenShareButton = () => {
+const constrains = {
+    audio: false,
+    video: true,
+};
+
+const ScreenShareButton = ({
+                               localStream,
+                               screenSharingStream,
+                               setSharingStream,
+                               isScreenSharingActive,
+                           }) => {
     const [isScreenSharingActive, setScreenSharingActive] = useState(false);
 
-    const handleScreenShareToggle = () => {
-        setScreenSharingActive(!isScreenSharingActive);
+    const handleScreenShareToggle = async () => {
+        if (!isScreenSharingActive) {
+            let stream = null;
+            try {
+                stream = await navigator.mediaDevices.getDisplayMedia(constrains);
+            } catch (err) {
+                console.log('Error on navigator.mediaDevices.getDisplayMedia');
+            }
+
+            if (!stream) {
+                setScreenSharingStream(stream);
+            } else {
+
+            }
+
+        }
     };
 
     return (
-        <IconButton onClick={handleScreenShareToggle} style={{ color: 'white' }}>
-            {isScreenSharingActive ? <ScreenShareIcon /> : <StopScreenShareIcon /> }
+        <IconButton onClick={handleScreenShareToggle} style={{color: 'white'}}>
+            {isScreenSharingActive ? <ScreenShareIcon/> : <StopScreenShareIcon/>}
 
         </IconButton>
     );
